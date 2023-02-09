f173d0283ef272759996035a71667567d1491aea
Weed <44820183+TodoshiTakato@users.noreply.github.com>
2023-02-09 18:52:45 +0500

    main branch: fixed to working sight

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,12 +18,10 @@ return new class extends Migration
             $table->charset = 'utf8mb4';
             $table->collation = 'utf8mb4_bin';
             $table->id();
-            $table->id();
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
             $table->string('username')->unique()->nullable();
             $table->string('email')->unique()->nullable();
-            $table->string('email')->unique();
             $table->string('email_verification_code')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->unsignedTinyInteger('is_active')->default(1);
